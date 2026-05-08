e6ce32bd113077cd3b6eef9306d281a31680440c
tadaura <sanshirou1110_0508@yahoo.co.jp>
2026-05-08 21:26:35 +0900

    TOPページでの記事一覧取得・記事詳細ページでの指定記事取得

---
+++ src/components/layouts/PublicHeader.tsx
@@ -0,0 +1,43 @@
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  NavigationMenu,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu";
+
+export default function PublicHeader() {
+  return (
+    <div>
+      <header className="border-b bg-blue-200">
+        <div className="container mx-auto px-3 py-4 flex items-center justify-between">
+          <NavigationMenu>
+            <NavigationMenuList>
+              <NavigationMenuItem>
+                <NavigationMenuLink className="font-bold text-xl">
+                  <Link href="/dashboard" className="font-bold text-xl">
+                    Blog
+                  </Link>
+                </NavigationMenuLink>
+              </NavigationMenuItem>
+            </NavigationMenuList>
+          </NavigationMenu>
+          <div className="flex items-center gap-4">
+            <Input
+              placeholder="記事を検索..."
+              className="w-[200px] lg:w-[300px]"
+            />
+            <Button variant="outline">
+              <Link href="/login">ログイン</Link>
+            </Button>
+            <Button variant="outline">
+              <Link href="/register">登録</Link>
+            </Button>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
